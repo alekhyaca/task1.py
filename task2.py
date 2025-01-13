177c4ac2f5e58f42fc5104ccd414c257c69d4f0b
alekhyaca <alekhya16901@gmail.com>
2025-01-13 09:19:02 +0530

    mssge

---
+++ task2.py
@@ -0,0 +1,54 @@
+a=10
+b=5
+print(a+b)
+print(a-b)
+
+c=8.5
+d=2.0
+print(c*d)
+print(c/d)
+
+e=5
+f=3
+print(5%3)
+print(5**3)
+
+g=75
+if(g<=100 and g>=50):
+    print(True)
+
+h=10
+i=20
+j=30
+if(i>0 and j>0 and h>0):
+    print(True)
+    
+k=10
+l=5
+k=k-l
+k=k+l
+l=k-l
+print(k)
+print(l)
+
+m=4
+if(m%2==0):
+    print("Even")
+else:
+    print("Odd")
+    
+n=1000
+o=20
+p=n-(n*(o/100))
+print(p)
+
+q=15
+q+=10
+q-=5
+q*=2
+q/=3
+print(q)
+
+r=42
+if(r>=1 and r<=100):
+    print(True)
